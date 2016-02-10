6eec6de523e94bb4081c03aa434488c4a3b81191
Shigenobu Kimura <skimu@me.com>
2016-02-09 22:01:19 -0600

    comment and backslash escape.

--- lang/c/c/tak.c
+++ lang/c/c/tak.c
@@ -1,9 +1,18 @@
+/* trigraphs are only processed with -trigraph option (by cpp) */
+
+/* with cpp, you can break line within whitespaces */
+\
+
+/* but you can not place comment after backslash */
+
 tak(x, y, z)
 {
   if (x <= y) 
     return z;
   else 
-    return tak(tak(x-1, y, z),
+    /*  backslash is processed by cpp. whitespaces after backslash is ignored (with warnings) */
+    re\
+turn tak(tak(x-1, y, z),
                tak(y-1, z, x),
                tak(z-1, x, y));
-}
+%>
